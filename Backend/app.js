9bc8a7b3b0adbbf2883e7825ad3ae09a148f4688
sohailansari9136@gmail.com <sohailansari9136@gmail.com>
2025-03-04 00:01:38 +0530

    fixed some bugs

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,17 @@ const dotenv = require(`dotenv`);
 const cors = require(`cors`);
 dotenv.config();
 const connectToDb = require(`./db/db`);
-
+const userRoutes = require('./routes/user.routes')
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
 
 app.get(`/`, (req, res) => {
     res.send(`Hello world`);
 });
+
+// app.use('/user',userRoutes);
 
 module.exports = app
